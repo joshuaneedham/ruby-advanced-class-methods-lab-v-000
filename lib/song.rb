7e12133e736fcdbd24d92121ca2dad1c75407979
Joshua Needham <joshuabneedham@gmail.com>
2017-11-07 18:57:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
-  def self.alphabetical(song_name)
+  def self.alphabetical
     self.all.sort_by { |s| s.name }
   end
 
